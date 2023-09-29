a269459e2ef2b2f8b12fac8de35352f7f072065f
FredericoCaetano <frederico.caetano01@gmail.com>
2023-09-29 15:54:33 -0300

    main

---
+++ main.py
@@ -0,0 +1,19 @@
+from funcoes import FuncoesTrem
+
+
+i = 0
+while i != 5:
+    i = int(input("\nO que você deseja fazer?\n1 - Adicionar Vagão\n2 - Remover Vagão\n3 - Buscar Vagão por trem\n4 - "
+                  "Contar vagões\n5 - Sair\n\n"))
+    funcao = FuncoesTrem()
+
+    if i == 1:
+        nVagao = int(input("\nEntre com o número do vagão: "))
+        nTrem = int(input("Entre em qual trem será adicionado esse vagão: "))
+        print("\n\n")
+
+        funcao.adicionar_vagao(nVagao, nTrem)
+
+    if i == 4:
+        quantidade_vagoes = funcao.contar_vagoes()
+        print(f"Quantidade de vagões na composição: {quantidade_vagoes}")
